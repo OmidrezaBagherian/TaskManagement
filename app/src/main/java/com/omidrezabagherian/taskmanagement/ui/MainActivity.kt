52714e670c8563b13e521c58f7fe13b8835dde5a
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-04-14 15:50:57 +0430

    https://trello.com/c/vH7gPYxS - Add hilt in project

--- app/src/main/java/com/omidrezabagherian/taskmanagement/ui/MainActivity.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.omidrezabagherian.taskmanagement
+package com.omidrezabagherian.taskmanagement.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omidrezabagherian.taskmanagement.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
