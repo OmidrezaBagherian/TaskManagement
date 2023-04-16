215af5fce6b49653fea95443d1667ccddcda3078
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-04-16 22:09:22 +0430

    https://trello.com/c/Z2PyyhW4 - Add adapters and create item

---
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/done/DoneAdapter.kt
@@ -0,0 +1,47 @@
+package com.omidrezabagherian.taskmanagement.ui.done
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.omidrezabagherian.taskmanagement.databinding.ItemTaskBinding
+import com.omidrezabagherian.taskmanagement.domian.models.Task
+
+
+class DoneAdapter(private val detail: (Task) -> Unit) :
+    ListAdapter<Task, DoneAdapter.DoneViewHolder>(
+        object : DiffUtil.ItemCallback<Task>() {
+            override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+                return oldItem.id == newItem.id
+            }
+        }
+    ) {
+    inner class DoneViewHolder(
+        private val itemTaskBinding: ItemTaskBinding,
+        private val detail: (Task) -> Unit
+    ) : RecyclerView.ViewHolder(itemTaskBinding.root) {
+        fun bind(task: Task) {
+            itemTaskBinding.textViewTitle.text = task.title
+            itemTaskBinding.textViewStatus.text = task.statusTask.name
+            itemTaskBinding.textViewDescription.text = task.description
+            itemTaskBinding.root.setOnClickListener {
+                detail(task)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DoneViewHolder =
+        DoneViewHolder(
+            ItemTaskBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            detail
+        )
+
+    override fun onBindViewHolder(holder: DoneViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
