58fdaba5775dffff0a86c3e25a78811e27a3243d
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-04-16 20:36:52 +0430

    https://trello.com/c/YRKLU2Lc - Add model task entity in project

---
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/domian/models/Task.kt
@@ -0,0 +1,17 @@
+package com.omidrezabagherian.taskmanagement.domian.models
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_db")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    @ColumnInfo
+    val title: String,
+    @ColumnInfo
+    val description: String,
+    @ColumnInfo
+    val statusTask: String
+)
