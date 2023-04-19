a6d88d195096af41a7b1275358cb379d02f2c0e7
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-04-19 12:15:32 +0430

    https://trello.com/c/nJrtf36R - Init fab insert in project

--- app/src/main/java/com/omidrezabagherian/taskmanagement/ui/main/MainFragment.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/main/MainFragment.kt
@@ -3,6 +3,7 @@ package com.omidrezabagherian.taskmanagement.ui.main
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import com.omidrezabagherian.taskmanagement.R
 import com.omidrezabagherian.taskmanagement.databinding.ActivityMainBinding
@@ -20,11 +21,25 @@ class MainFragment: Fragment(R.layout.fragment_main) {
 
         binding = FragmentMainBinding.bind(view)
 
-        val adapter = ViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
-        binding.viewPager.adapter = adapter
+        setupUI()
+    }
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+    private fun setupUI(){
+        setupTabLayout()
+        setupFabInsert()
+    }
+    private fun setupTabLayout(){
+        val adapter = ViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+        binding.vpStatus.adapter = adapter
+
+        TabLayoutMediator(binding.tlStatus, binding.vpStatus) { tab, position ->
             tab.text = ConstValues.TAB_EN[position]
         }.attach()
+    }
+
+    private fun setupFabInsert(){
+        binding.fabInsert.setOnClickListener {
+            Snackbar.make(requireView(),"Insert Task",Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
