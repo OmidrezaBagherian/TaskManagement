7ae68b5c623c264878267be949197cc8e413f424
OmidrezaBagherian <OmidCoder21@gmail.com>
2023-04-21 16:47:46 +0430

    https://trello.com/c/ikAn5LoC - Add function find task by id in dao

--- app/src/main/java/com/omidrezabagherian/taskmanagement/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/detail/DetailFragment.kt
@@ -1,9 +1,48 @@
 package com.omidrezabagherian.taskmanagement.ui.detail
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.navArgs
 import com.omidrezabagherian.taskmanagement.R
+import com.omidrezabagherian.taskmanagement.databinding.FragmentDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+    private lateinit var binding: FragmentDetailBinding
+    private val detailViewModel: DetailViewModel by viewModels()
+    private val navArgs: DetailFragmentArgs by navArgs()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentDetailBinding.bind(view)
+
+        setupUI()
+
+    }
+
+    private fun setupUI() {
+        bindDataToUI(navArgs.task.id)
+    }
+
+    private fun bindDataToUI(id: Int) {
+        detailViewModel.taskById(id)
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.CREATED) {
+                detailViewModel.detail.collect { data ->
+                    binding.mtvTitle.text = data.title
+                    binding.mtvStatus.text = data.taskStatus.name
+                    binding.mtvDescription.text = data.description
+                }
+            }
+        }
+    }
+
 }
