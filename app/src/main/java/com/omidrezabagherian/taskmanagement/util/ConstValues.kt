c20a9bc311af16703d67f1482559531d3251aca9
OmidrezaBagheriyan <OmidCoder21@gmail.com>
2023-06-10 00:46:40 +0330

    https://trello.com/c/NkKUtwhL - Remove list tabs to main fragment

--- app/src/main/java/com/omidrezabagherian/taskmanagement/util/ConstValues.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/util/ConstValues.kt
@@ -2,10 +2,4 @@ package com.omidrezabagherian.taskmanagement.util
 
 object ConstValues {
     const val NUM_TABS = 3
-    val TAB_FA = arrayListOf<String>(
-        "کارها","درحال انجام","اتمام"
-    )
-    val TAB_EN = arrayListOf<String>(
-        "Task","Doing","Done"
-    )
 }
