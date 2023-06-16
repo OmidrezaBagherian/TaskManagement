785215ef66767222174dacd8e4b57de80765eff6
OmidrezaBagheriyan <OmidCoder21@gmail.com>
2023-06-17 02:31:15 +0330

    https://trello.com/c/NkKUtwhL - add menu in main fragment

--- app/src/main/java/com/omidrezabagherian/taskmanagement/ui/main/MainFragment.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/main/MainFragment.kt
@@ -1,15 +1,20 @@
 package com.omidrezabagherian.taskmanagement.ui.main
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
 import com.omidrezabagherian.taskmanagement.R
 import com.omidrezabagherian.taskmanagement.databinding.FragmentMainBinding
 import com.omidrezabagherian.taskmanagement.ui.ViewPagerAdapter
-import com.omidrezabagherian.taskmanagement.util.ConstValues
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,9 +26,14 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         findNavController()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //setHasOptionsMenu(true)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
         binding = FragmentMainBinding.bind(view)
 
@@ -33,6 +43,33 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private fun setupUI() {
         setupTabLayout()
         setupFabInsert()
+        setupMenu()
+    }
+
+    private fun setupMenu() {
+        val menuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_main, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_setting -> {
+                        Toast.makeText(requireContext(), "Setting", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    R.id.menu_about -> {
+                        Toast.makeText(requireContext(), "About", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     private fun setupTabLayout() {
@@ -51,4 +88,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             navController.navigate(MainFragmentDirections.actionMainFragmentToInsertFragment())
         }
     }
+
+    /*override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_app, menu)
+    }*/
 }
