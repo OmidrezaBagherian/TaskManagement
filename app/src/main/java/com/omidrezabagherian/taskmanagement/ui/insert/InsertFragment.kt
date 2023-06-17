de2f82deb579846bf869dc01234007aed2a310ab
OmidrezaBagheriyan <OmidCoder21@gmail.com>
2023-06-18 03:14:38 +0330

    https://trello.com/c/NkKUtwhL - Update insert fragment

--- app/src/main/java/com/omidrezabagherian/taskmanagement/ui/insert/InsertFragment.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/insert/InsertFragment.kt
@@ -2,6 +2,7 @@ package com.omidrezabagherian.taskmanagement.ui.insert
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -31,23 +32,35 @@ class InsertFragment : Fragment(R.layout.fragment_insert) {
 
     private fun setupUI() {
         setupBtnInsert()
+        setAdapterAutoComplete()
     }
 
-    private fun getStatus(): TaskStatus =
-        when (binding.rgStatus.checkedRadioButtonId) {
-            R.id.rbTask -> {
+    private fun setAdapterAutoComplete() {
+        val status = resources.getStringArray(R.array.text_insert_status)
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.select_dialog_item, status)
+        binding.actvStatus.setAdapter(adapter)
+    }
+
+    private fun getStatus(): TaskStatus {
+        val status:Array<String> = resources.getStringArray(R.array.text_insert_status)
+        return when (binding.actvStatus.text.toString()) {
+            status[0] -> {
                 TaskStatus.TASK
             }
-            R.id.rbDoing -> {
+
+            status[1] -> {
                 TaskStatus.DOING
             }
-            R.id.rbDone -> {
+
+            status[2] -> {
                 TaskStatus.DONE
             }
+
             else -> {
                 TaskStatus.NONE
             }
         }
+    }
 
     private fun insertTaskToDatabase(task: Task) {
         insertViewModel.insertTask(task)
