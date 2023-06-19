d9e73ba729516a5c5c56bd4412b5c1f7a676266f
OmidrezaBagheriyan <OmidCoder21@gmail.com>
2023-06-20 01:33:06 +0330

    https://trello.com/c/ACjnD6kZ - Add about me fragment

---
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/about/AboutMeFragment.kt
@@ -0,0 +1,7 @@
+package com.omidrezabagherian.taskmanagement.ui.about
+
+import androidx.fragment.app.Fragment
+import com.omidrezabagherian.taskmanagement.R
+
+class AboutMeFragment : Fragment(R.layout.fragment_about_me) {
+}
