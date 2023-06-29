2a255f357188fc15c3bee2d503aaa6415d68fb52
OmidrezaBagheriyan <OmidCoder21@gmail.com>
2023-06-29 11:54:39 +0330

    https://trello.com/c/ACjnD6kZ - Add about me view model and edit fragment

--- app/src/main/java/com/omidrezabagherian/taskmanagement/ui/about/AboutMeFragment.kt
+++ app/src/main/java/com/omidrezabagherian/taskmanagement/ui/about/AboutMeFragment.kt
@@ -2,6 +2,8 @@ package com.omidrezabagherian.taskmanagement.ui.about
 
 import androidx.fragment.app.Fragment
 import com.omidrezabagherian.taskmanagement.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AboutMeFragment : Fragment(R.layout.fragment_about_me) {
 }
